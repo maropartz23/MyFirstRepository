76f15692a3677542e6659f3ae75569324214f27d
Martin Ropartz <martin.ropartz@etu.imt-nord-europe.fr>
2024-09-18 15:42:25 +0100

    version amélioré dont je ne comprends pas les problèmes

--- ROPARTZMartin.c
+++ ROPARTZMartin.c
@@ -1,30 +1,36 @@
 //Martin ROPARTZ
 
 #include <stdio.h>
+#include <stdlib.h>
+
+
 
 int* repeat(int t1[],int t2[], int tailletableau){
-    int *p;
     int somme=0;
     for (int i=0; i< tailletableau ;i++){
             somme += t1[i];
     }
-    int tab[somme];
+    int * res= malloc(somme*sizeof(int)); 
+    int current = 0;
     for (int i=0;i< tailletableau; i++){
         for(int j=0; j<t1[i];j++){
-            tab[i+j]=t2[i];
+            res[current]=t2[i];
+            current++;
         }
     }
-    //for (int i=0; i< somme; i++){
-    p=tab;
-    //}
-     
-    return p;
+   
+    return res;
     
 }
 
-int * main(void){
+int main(void){
     int t1[3]={1,7,2};
     int t2[3]={1,2,3};
-    return repeat(t1,t2,3);
+    int * res = repeat(t1,t2,3);
+    printf(res);
+
+    free(res);
+    return 0;
 
 }
+
