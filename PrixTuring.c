0f31cc5114cb87c7b85b4f19b695f2da1a54e968
Martin Ropartz <martin.ropartz@etu.imt-nord-europe.fr>
2024-09-20 09:03:41 +0100

    number of winners

--- PrixTuring.c
+++ PrixTuring.c
@@ -4,27 +4,57 @@
 #include <errno.h>
 #include <assert.h>
 
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-// MAIN
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-int main(int argc, char** argv){
-
-    
-    struct prixTuring{
-        int annee;
-        char nom;
-        char nature;
+ struct prixTuring{
+        unsigned short annee;
+        char * nom;
+        char * nature;
     };
 
     typedef struct prixTuring turing;
 
+
+int numberOfWinners(FILE * f){
+    int nbWinners=0;
+    char buffer[1024];
+    while (feof(f)==0){
+        fgets(buffer,1024,f);
+        nbWinners++;
+    }
+    return nbWinners;
     
+}
+
+int main(int argc, char** argv){
 
 	char filename[] = "turingWinners.csv";
-	char outputFilename[] = "out.csv";
+    char outputFilename[] = "out.csv";
+    FILE* f;
+    FILE* output;
 
-    // TODO
+    f = fopen(filename, "r");
+    output = fopen(outputFilename, "w");
+    char * line;
+    
+
+    if(f==NULL){
+        printf("Code de l'erreur: %d\n",  errno);
+    }
+    else{
+        while (feof(f)==0)
+        {
+            char lettre = fgetc(f);
+            putc(lettre, output);
+            
+        }
+        
+    }
+
+    int winner = numberOfWinners(f);
+    printf("le nombre de vainceur est: %d",winner);
+
+    fclose(f);
+    fclose(output);
 
 	return EXIT_SUCCESS;
 }
+
