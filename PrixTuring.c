b0aed71c649cc09383e1078ddb4fa752fcc8d862
Martin Ropartz <martin.ropartz@etu.imt-nord-europe.fr>
2024-09-20 11:12:11 +0100

    version intermédiaire

--- PrixTuring.c
+++ PrixTuring.c
@@ -24,6 +24,51 @@ int numberOfWinners(FILE * f){
     
 }
 
+turing * readWinners(int numberOfTuringWinner,FILE * f){ //fonction à refaire avec les bons arguments 
+    turing * winners = calloc(numberOfTuringWinner,sizeof(turing));
+    for(int i = 0; i < numberOfTuringWinner; ++i)
+        readwinner(&winners[i],f)  //&winners[i] -> adresse d'une struct  et   readwinner à écrire 
+
+    return winners;
+
+    // en dessous partie à revoir  -> fonction readWinner? 
+    while(feof(f)==0){
+        char buffer[1024];
+        fgets(buffer,1024,f);
+        int k=0;
+        
+        while (buffer[k]=="\0"){
+            int l=0;
+            int m=0;
+            
++
+
+            for(int i=0; i<2;i++){
+                //mettre une condition si i=0 car int et non char * 
+                while(buffer[l]==";"){
+                    l++;    
+                    }
+                char *infoStruct=malloc((l-m)*sizeof(char))
+                for (int j=m;j<l-m;j++ ){
+                    infoStruct[j]=buffer[j];
+                }
+                if(i==1):{
+                    turing->nom= infoStruct;
+                }
+                else if (i==2)
+                {
+                    turing->nature=infoStruct;
+                }
+                
+                m=l;
+                l++;
+            }
+                
+        }
+    }    
+}
+
+
 int main(int argc, char** argv){
 
 	char filename[] = "turingWinners.csv";
@@ -51,6 +96,10 @@ int main(int argc, char** argv){
 
     int winner = numberOfWinners(f);
     printf("le nombre de vainceur est: %d",winner);
+
+    turing *winners=readWinners(winner, f);
+
+    
 
     fclose(f);
     fclose(output);
